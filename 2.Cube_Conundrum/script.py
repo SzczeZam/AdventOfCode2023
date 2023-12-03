be8125e9ea6cf0f3731c2753ce00a84c217cb6af
Szczezam <wookslandline@protonmail.com>
2023-12-03 11:46:02 -0500

    day2: part 2 complete

--- 2.Cube_Conundrum/script.py
+++ 2.Cube_Conundrum/script.py
@@ -17,7 +17,7 @@ def getValues(arr):
     blue = 0
     isValid= True
     for item in arr:
-        cNu = int(re.findall("(\d+)\s", item)[0])
+        cNu = int(re.findall("(\\d+)\\s", item)[0])
         val= item[-1]
         match val:
             case "r":
@@ -39,21 +39,38 @@ def getValues(arr):
 
 def getGameData(gameStr):
     vars = re.split(":",gameStr)
-    id = re.findall("(\d+):",gameStr)[0]
+    id = re.findall("(\\d+):",gameStr)[0]
     throws = re.split(";",vars[1])
     gameObj = {
         "id":id,
         "throw_amount" : len(throws),
         "throw_list" : [],
-        "isValid" : True 
+        "isValid" : True, 
+        "minimum_set": []
     }
-
+    hiR = 0
+    hiG = 0
+    hiB = 0
     for i in range(0, len(throws)):
-        values = re.findall("(\d+\s\w)", throws[i])
+        values = re.findall("(\\d+\\s\\w)", throws[i])
         valueArr = getValues(values)
         gameObj["throw_list"].append(valueArr)
+        if (valueArr[0] > hiR):
+            hiR = valueArr[0]
+        else:
+            hiR=hiR
+        if (valueArr[1] > hiG):
+            hiG = valueArr[1] 
+        else:
+            hiG=hiG
+        if (valueArr[2] > hiB):
+            hiB = valueArr[2] 
+        else:
+            hiG=hiG
         if (valueArr[-1] == False):
                gameObj["isValid"]= False
+    gameObj["minimum_set"] = [ hiR, hiG, hiB ]
+    gameObj["power"] = ( hiR*hiG*hiB )
     return gameObj
 
 def parseData():
@@ -61,21 +78,24 @@ def parseData():
     with open(data, "r") as input:
         invalid_ids = []
         valid_ids = []
+        power_list = []
         for line in input:
            gameData = getGameData(line)
            if (gameData["isValid"] == False):
                invalid_ids.append(int(gameData["id"]))
            elif (gameData["isValid"] == True):
                valid_ids.append(int(gameData["id"]))
+           power_list.append(gameData["power"])
 
         print(f"{len(invalid_ids)} invalid games.")
         print(f"{len(valid_ids)} valid games.")
+        print(f"{len(power_list)} minimum_set powers collected.\n")
 
         valid_sum = sum(valid_ids)
         invalid_sum = sum(invalid_ids)
-        
-        print("sum of valid ids")
-        print(valid_sum)
+        power_sum = sum(power_list)
+        print(f"sum of valid ids: {valid_sum}")
+        print(f"sum of minimum_set powers: {power_sum}")
 
 
 

87cb1e865953f2fa69beb8492499146bde5b578c
Szczezam <wookslandline@protonmail.com>
2023-12-05 21:29:16 -0500

    a crash course in GLF and git reset

--- 4.Scratchcards/card.py
+++ 4.Scratchcards/card.py
@@ -10,15 +10,27 @@ value_list = [[filter(str.strip, res[0].split(' ')), filter(str.strip, res[1].sp
 t_list = [ set(card[0]) & set(card[1]) for card in value_list]
 win_amount_list = [ len(i) for i in t_list ]
 
-points = []
-for wins in win_amount_list:
-    value = 1 if wins > 0 else 0 
-    for win in range(0,wins-1):
-        print(value)
-        value=value*2
-    points.append(value)
+#    points = []
+#    for wins in win_amount_list:
+#        value = 1 if wins > 0 else 0 
+#        for win in range(0,wins-1):
+#            print(value)
+#            value=value*2
+#        points.append(value)
 
-print(win_amount_list)
-print(points)
 
-print(sum(points))
+cards = [1 for card in input_lines]
+for card in range(0, len(cards)):
+    print(f"card {card + 1}:{win_amount_list[card]} wins")
+    winwin = win_amount_list[card]
+    for won_cards in range(0,cards[card]):
+        for i in range(1,win_amount_list[card]+1):
+            ind = card+i
+            print(f"adding one to card {ind+1} : {cards[ind]} +1 ")
+            cards[ind] = cards[ind] + 1
+
+print(cards)
+print(len(win_amount_list))
+#print(len(points))
+#print(sum(points))
+print(sum(cards))

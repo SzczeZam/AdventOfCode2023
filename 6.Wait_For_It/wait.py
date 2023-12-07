e0834dd237fd11be5fe11ad4157a908aca1431d9
Szczezam <wookslandline@protonmail.com>
2023-12-06 19:16:32 -0500

    day 6 pt2 dummy data passed

--- 6.Wait_For_It/wait.py
+++ 6.Wait_For_It/wait.py
@@ -3,14 +3,16 @@ import sys
 from functools import reduce
 
 data = sys.argv[1]
-#flag = sys.argv[2] 
+flag = sys.argv[2] 
 
 input_lines = [ re.findall(r'(\d+)', line) for line in open(data, "r")]
 
-times = input_lines[0]
-dists = input_lines[1]
+times = input_lines[0] if flag == '1' else ''.join(input_lines[0]) 
+dists = input_lines[1] if flag == '1' else ''.join(input_lines[1]) 
+print( times, dists )
 
-ww = [len([s for s in range(0,int(times[t])) if (s*(int(times[t])-s)>int(dists[t]))]) for t in range(0,len(times))]
-product = reduce(lambda x,y : x*y, ww)
+ww = [len([s for s in range(0,int(times)) if (s*(int(times)-s)>int(dists))])]
+
+#product = reduce(lambda x,y : x*y, ww)
 print(ww)
-print(product)
+#print(product)
